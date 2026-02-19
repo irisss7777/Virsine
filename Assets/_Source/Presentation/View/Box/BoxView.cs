59b102a8a8a2368a3bcdbcb75e53233120cc4aa5
zelenin.m <irbissssss@net.com>
2026-02-19 15:51:07 +0700

    Fix boxView

--- Assets/_Source/Presentation/View/Box/BoxView.cs
+++ Assets/_Source/Presentation/View/Box/BoxView.cs
@@ -10,12 +10,13 @@ namespace Presentation.View.Box
         public bool PickupMod { get; private set; }
         public Rigidbody Rigidbody => GetComponent<Rigidbody>();
 
-        [SerializeField] private GameObject _collidersObject;
-        
         public event Action<bool, Collider> OnTriggerChanged;
         public event Action OnDestroyBox;
+        
+        [SerializeField] private GameObject _collidersObject;
 
-        public void SetPickupMod(bool pickupMod) => PickupMod = pickupMod;
+        public void SetPickupMod(bool pickupMod) => 
+            PickupMod = pickupMod;
 
         public void SetColliderEnable(bool isEnable) =>
             _collidersObject.SetActive(isEnable);
@@ -23,15 +24,10 @@ namespace Presentation.View.Box
         public void DestroyBox() =>
             OnDestroyBox?.Invoke();
 
-        private void OnTriggerEnter(Collider other)
-        {
+        private void OnTriggerEnter(Collider other) => 
             OnTriggerChanged?.Invoke(true, other);
-        }
 
-        private void OnTriggerExit(Collider other)
-        {
+        private void OnTriggerExit(Collider other) =>
             OnTriggerChanged?.Invoke(false, other);
-
-        }
     }
 }
